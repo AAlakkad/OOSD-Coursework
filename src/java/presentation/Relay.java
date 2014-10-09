7bc719376a2e0f94d35867bdea77a47051b5f795
Ammar Alakkad <am.alakkad@gmail.com>
2014-10-09 23:47:31 +0300

    Relay: successfully forward request to Login servlet

--- src/java/presentation/Relay.java
+++ src/java/presentation/Relay.java
@@ -7,6 +7,7 @@ package presentation;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,19 +29,24 @@ public class Relay extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Relay</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Relay at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        String action = request.getParameter("action");
+        
+        if (action != null) {
+            RequestDispatcher rd = request.getRequestDispatcher("/");
+            switch (action) {
+                case "Login":
+                    rd = request.getRequestDispatcher(action);
+                    break;
+            }
+            if (rd != null) {
+                rd.forward(request, response);
+                // output server message indicating forward has been done
+                System.out.println("Request forwarded to " + action + " servlet");
+            }
+
         }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
