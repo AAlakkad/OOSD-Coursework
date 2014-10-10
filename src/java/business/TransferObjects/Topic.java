d3d5514969382104fc537aae524a5a6a10f88e66
Ammar Alakkad <am.alakkad@gmail.com>
2014-10-11 01:32:47 +0300

    TO: add basic Topic interface and class

---
+++ src/java/business/TransferObjects/Topic.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.TransferObjects;
+
+/**
+ *
+ * @author ammar
+ */
+public class Topic implements TopicInterface {
+
+    private String name, description;
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
---
+++ src/java/business/TransferObjects/TopicInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.TransferObjects;
+
+/**
+ *
+ * @author ammar
+ */
+public interface TopicInterface {
+    String getName();
+    void setName(String name);
+    String getDescription();
+    void setDescription(String description);
+}
