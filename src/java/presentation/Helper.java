48361a130c9293a50204e65a7d1ef2acda8c9e2d
Ammar Alakkad <am.alakkad@gmail.com>
2014-10-18 07:23:24 +0300

    Add Helper class, containing method to convert numbers to ordinals (1st, 2nd, etc.)

---
+++ src/java/presentation/Helper.java
@@ -0,0 +1,35 @@
+package presentation;
+
+/**
+ *
+ * @author ammar
+ */
+public class Helper {
+
+    /**
+     * Get string representation of a number, e.g. 1st, 2nd, 3rd, 4th, etc.
+     *
+     * @param contestantScore
+     * @return String
+     *
+     * Original source: http://www.javalobby.org/forums/thread.jspa?threadID=16906&tstart=0
+     */
+    public static String getOrdinalFor(Integer contestantScore) {
+        int hundredRemainder = contestantScore % 100;
+        int tenRemainder = contestantScore % 10;
+        String suffix = "th";
+        if (hundredRemainder - tenRemainder != 10) {
+
+            switch (tenRemainder) {
+                case 1:
+                    suffix = "st";
+                case 2:
+                    suffix = "nd";
+                case 3:
+                    suffix = "rd";
+            }
+        }
+        return contestantScore + suffix;
+    }
+
+}
